32e8de5c5ac79716f95fd6cac4932d562a5e71a3
樊星 <fxyun@vip.qq.com>
2021-04-26 16:34:55 +0800

    搭建好Eureka注册中心

---
+++ Eureka-9901/src/main/java/cn/l404/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package cn.l404.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @auther Fanxing
+ * SpringBoot快捷创建启动类
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}    
